10069382d74a706c6a99de5534d830084f84dac8
Chirag <chiragmunjal550@gmail.com>
2024-06-02 04:01:09 +0530

    Uploaad the files to S3(AWS)

---
+++ vercel-upload-service/src/aws.ts
@@ -0,0 +1,23 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+  accessKeyId: "ed29f2842da94dcfe242d7b21afe7d5b",
+  secretAccessKey:
+    "965bce1c2d916332e0b1dccb69c61b0fcac073fdbc333f9f4df751a7e579d3be",
+  endpoint: "https://fccb72b040c1e4466774e912731b5e37.r2.cloudflarestorage.com",
+});
+
+// fileName => output/12312/src/App.jsx
+// filePath => C:\Users\Chira\OneDrive\Desktop\Vercel_Clone\vercel-upload-service\dist\output\12312\src\App.jsx
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+  const fileContent = fs.readFileSync(localFilePath);
+  const response = await s3
+    .upload({
+      Body: fileContent,
+      Bucket: "vercel",
+      Key: fileName,
+    })
+    .promise();
+  console.log(response);
+};
--- vercel-upload-service/src/index.ts
+++ vercel-upload-service/src/index.ts
@@ -1,3 +1,6 @@
+//id -ed29f2842da94dcfe242d7b21afe7d5b
+//secret - 965bce1c2d916332e0b1dccb69c61b0fcac073fdbc333f9f4df751a7e579d3be
+//endpoint -https://fccb72b040c1e4466774e912731b5e37.r2.cloudflarestorage.com
 import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
@@ -5,18 +8,21 @@ import { generate } from "./utils";
 import { getAllFiles } from "./file";
 
 import path from "path";
+import { uploadFile } from "./aws";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 console.log("server is running");
+console.log(__dirname);
 
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
 
   //generating a random id
   const id = generate(); // asd12
+  console.log(__dirname);
 
   //clone a git repo in local
   await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
@@ -26,6 +32,9 @@ app.post("/deploy", async (req, res) => {
   const files = getAllFiles(path.join(__dirname, `output/${id}`));
 
   //upload to s3(aws)
+  files.forEach(async (file) => {
+    await uploadFile(file.slice(__dirname.length + 1), file);
+  });
 
   //publish in redis queue
 
